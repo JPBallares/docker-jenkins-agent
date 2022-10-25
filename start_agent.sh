181ff094b2a7a91d2da7ae1a1afca05fc0d92c05
JPBallares <32734321+JPBallares@users.noreply.github.com>
2022-10-25 15:46:13 +0800

    Update start_agent.sh

--- start_agent.sh
+++ start_agent.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+# values here can be taken from the jenkins when adding new node
+# you can follow this link for setup https://adamtheautomator.com/jenkins-agent/
 SECRET=eb67cb3478d21f605a4381336795efcea6c6bb56053227f3210c51500874b8bb
 AGENT_JAR_URL=http://ec2-13-250-20-153.ap-southeast-1.compute.amazonaws.com/jnlpJars/agent.jar
 CONTROLLER_URL=http://ec2-13-250-20-153.ap-southeast-1.compute.amazonaws.com/computer/main%2Dagent/jenkins-agent.jnlp
 WORK_DIR=/var/jenkins/agent
-
 
 curl -sO $AGENT_JAR_URL
 java -jar agent.jar -jnlpUrl $CONTROLLER_URL -secret $SECRET -workDir "$WORK_DIR"
